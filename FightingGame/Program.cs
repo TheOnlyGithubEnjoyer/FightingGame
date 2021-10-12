36a2478f34bcdc1b8100fd50f6522dcea1336d1f
Elliot Walker <Elliot.Walker@elev.ga.ntig.se>
2021-10-12 10:42:29 +0200

    bruh

---
+++ FightingGame/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+int HP1 = 100;
+
+int HP2 = 100;
+
+Console.WriteLine("Player 1. Write your name");
+string name1;
+name1 = Console.ReadLine();
+
+Console.WriteLine("Player 2. Write your name");
+string name2;
+name2 = Console.ReadLine();
+
+Console.WriteLine(name1 + "'s Health = " + HP1);
+Console.WriteLine(name2 + "'s Health = " + HP2);
+Console.WriteLine("PRESS SPACE TO KNOCK HIS ASS OUT!");
+
+Console.ReadLine();
+
+while (HP1 > 0 && HP2 > 0)
+{
+
+    Random generator = new Random();
+
+    int damage1 = generator.Next(8,20);
+
+    int damage2 = generator.Next(8,20);
+
+    HP1 = HP1-damage2;
+    HP2 = HP2-damage1;
+
+    Console.WriteLine(name1 + "'s Health: " + HP1 + "! " + name2 + " Did " + damage2 + " on yo stupid ass!");
+    Console.WriteLine(name2 + "'s Health: " + HP2 + "! " + name1 + " Did " + damage1 + " on you!");
+    Console.WriteLine("PRESS SPACE TO ATTACK");
+    
+
+    Console.ReadLine();
+
+}
+
+if (HP1 < 0 && HP2 < 0)
+{
+    Console.WriteLine("Ohhh my! It's a fucking draw. What are the chances.");
+}
+
+else if(HP1 < 0 || HP1 == 0)
+{
+    Console.WriteLine(name2 + " has beaten " + name1 + " easily! " + "With only " + HP2 + "HP left");
+}
+
+else if(HP2 < 0 || HP2 == 0)
+{
+    Console.WriteLine(name1 + " has beaten " + name2 + " like a baby " + "With only " + HP1 + "HP left");
+}
+
+Console.ReadLine();
+
+
+
